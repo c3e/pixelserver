83d21de12d88efd5c4b9f009942b9fa19caa6ab6
a3x <ad@er1s.tk>
2017-06-03 05:49:38 +0200

    changes

--- init.sh
+++ init.sh
@@ -1,2 +1,17 @@
-git clone https://github.com/eclipse/mosquitto libs/mosquitto
+#!/bin/bash
+#
+
+#
+# Compiling static libmosquitto only works on develop branch as of now 
+# but develop is fucking unstable, so this is greyed out
+#
+# git clone https://github.com/eclipse/mosquitto libs/mosquitto
+# cd libs/mosquitto
+# git checkout develop
+# cd ../..
+
+# no need for dependency
+# build libevent
 git clone https://github.com/libevent/libevent libs/libevent
+mkdir libs/libevent/build
+cmake -B../libs/libevent/build/ -H../libs/libevent/
